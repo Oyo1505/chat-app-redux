1c18b664ba325f2521873e2af33ccf0794f0ca29
RIGOULET Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2019-10-05 11:06:23 +0200

    fisrt commit
    initial commit

---
+++ ChatApp/ChatApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+ class ChatApp extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>ChatApp</div>
+		);
+	}
+}
+export default ChatApp;
